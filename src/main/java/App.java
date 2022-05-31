a8834db65f8a0c020c307a960f0f1887d93505ea
Anton Shadrin <antonbest2@yandex.ru>
2022-05-31 18:43:32 +0400

    Kata 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,20 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean b1= bean==bean2;
+
+        boolean b2= bean3==bean4;
+
+        System.out.println("Сравнение первой пары: "+b1);
+        System.out.println("Сравнение второй пары: "+b2);
+
+        //System.out.println(bean.getMessage());
     }
 }
